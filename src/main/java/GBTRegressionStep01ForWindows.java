57cb7ae4cd719bc14d6f5d51e12532a8379dc789
riversun <riversun.org@gmail.com>
2019-08-15 08:37:43 +0900

    Add comments

--- src/main/java/GBTRegressionStep01ForWindows.java
+++ src/main/java/GBTRegressionStep01ForWindows.java
@@ -1,4 +1,26 @@
 
+/* 
+ *  Copyright (c) 2019 Tom Misawa, riversun.org@gmail.com
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *  
+ */
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -8,7 +30,7 @@ public class GBTRegressionStep01ForWindows {
   public static void main(String[] args) {
 
     System.setProperty("hadoop.home.dir", "c:\\Temp\\winutil\\");
-    
+
     SparkSession spark = SparkSession
         .builder()
         .appName("GradientBoostingTreeGegression")
@@ -22,7 +44,7 @@ public class GBTRegressionStep01ForWindows {
         .format("csv")// (3)
         .option("header", "true")// (4)
         .option("inferSchema", "true")// (5)
-        .load("dataset/gem_price_ja.csv");// (6)
+        .load("dataset/gem_price.csv");// (6) // gem_price_ja.csv for Japanese
 
     dataset.show();// (7)
 
